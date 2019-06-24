cf7207107b815dbc8cd7bf378e3ed103ed1152c1
roskata729 <36508455+roskata729@users.noreply.github.com>
2019-06-25 01:47:05 +0300

    Add files via upload

---
+++ NBAplayers/ConsoleApp3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+
+
+namespace ConsoleApp3
+{
+
+    class Program
+    {
+        public static List<Player> JsonReader(string myPath) {
+            
+
+            using (StreamReader r = new StreamReader(myPath))
+            {
+                string json = r.ReadToEnd();
+                List<Player> items = JsonConvert.DeserializeObject<List<Player>>(json);
+                
+                return items;
+
+                
+            }
+        }
+
+        public class Player
+        {
+            public string Name { get; set; }
+            public int PlayingSince { get; set; }
+            public string Position { get; set; }
+            public int Rating { get; set; }
+
+            public Player()
+            {
+
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert Json file path: ");
+            List<Player> allPlayers = Program.JsonReader(Console.ReadLine());
+            Console.WriteLine("Years to qualify: ");
+            int yearsToQualify = int.Parse(Console.ReadLine());
+            Console.WriteLine("Minimum rating: ");
+            int minRating = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert CSV file path:");
+            string pathToCSV = Console.ReadLine();
+            
+
+            List<Player> qualifiedPlayers = new List<Player>();
+
+            foreach (var player in allPlayers) {
+                if (player.Rating >= minRating) {
+                    if (DateTime.Now.Year - player.PlayingSince <= yearsToQualify) {
+                        qualifiedPlayers.Add(player);
+                    }
+                }
+            }
+
+            using (StreamWriter writeToCSV = new StreamWriter(pathToCSV)) {
+                writeToCSV.WriteLine("Name, Rating");
+                foreach (var player in qualifiedPlayers.OrderByDescending(x=>x.Rating)) {
+                    writeToCSV.WriteLine($"{player.Name}, {player.Rating}");
+                }
+            }
+
+            Console.WriteLine("Sorting completed");
+            
+        }
+    }
+}
